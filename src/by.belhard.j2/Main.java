5074de4b87b6fc322640386ad3e13f4d770e3f20
Soilofgod <Lexxxa5600>
2019-04-09 23:22:54 +0300

    1.1

---
+++ src/by.belhard.j2/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+
+                for (int a=0;a<25;a+=4){
+                    System.out.println(a);
+                }
+            }
+}
--- src/lesson2/jump/da/fuck/up.java
+++
@@ -1,4 +0,0 @@
-package lesson2.jump.da.fuck;
-
-public class up {
-}
