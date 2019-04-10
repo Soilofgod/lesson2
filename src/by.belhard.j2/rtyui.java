c6a2f81178981ea0972b77c42542591eb2dcfe90
Soilofgod <Soul-god@mail.ru>
2019-04-10 19:01:42 +0300

    1.1

---
+++ src/by.belhard.j2/rtyui.java
@@ -0,0 +1,25 @@
+package by.belhard.j2;
+
+public class rtyui {
+    public static void main(String[] args){
+        a1();
+        a2();
+        a3();
+}
+   static void a1(){
+    int x=2;
+    x=x*10;
+       System.out.println(x);
+    }
+static void a2() {
+    int f = 45;
+    f = f * 6;
+    System.out.println(f);
+}
+    static void a3(){
+        int g=90;
+        g=g*36;
+        System.out.println(g);
+    }
+
+}
